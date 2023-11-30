401b1e8f461c6b7f5bede7a8bdda6ab737e89905
Luan Nguyen <tailuan30092000@gmail.com>
2023-11-29 18:36:22 -0800

    Solved simple 2d dynamic programming

--- cmpt307.py
+++ cmpt307.py
@@ -1,5 +1,6 @@
 import pdb
 import typing
+from pprint import pformat
 
 
 class Solution:
@@ -15,3 +16,37 @@ class Solution:
             if (i >= j):
                 break
         return boat
+    
+    def printdp(self, dp: typing.List[int]) -> None:
+        for i,value in enumerate(dp):
+            print(i, value)
+
+    def subsetSum(self, S: typing.List[int], t: int) -> tuple:
+        """
+        Let S be a set of n dist. pos. int., and let t be a pos. int. Devise an algo. that determines, with O(nt) operations in the worst case
+        if there exists a subset of S whose element total t
+
+        @params
+            S: list of distinct, positive integers
+            total: a positive int
+        @return
+            True if its existed a subset of S that its sum is equalled to t
+        """
+        
+        S.sort()
+        S_len = len(S)
+        dp = [[False for _ in range(t+1)] for _ in range(S_len+1)]
+        for i in range(S_len+1):
+            dp[i][0] = True
+        for i in range(1, S_len+1):         # i is first ith coin you can use to make j
+            for j in range(1, t+1):         # j is sum value
+                if S[i-1] <= j:             # check out of range for dp[i-1][j-S[i-1]]
+                    dp[i][j] = dp[i-1][j] or dp[i-1][j-S[i-1]]
+                else:
+                    dp[i][j] = dp[i-1][j]
+                # pdb.set_trace()
+        self.printdp(dp)
+
+        return dp[S_len][t]
+    
+
--- cmpt307_test.py
+++ cmpt307_test.py
@@ -11,5 +11,9 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(self.solution.lifeBoat([2, 4, 6, 8, 10], 10), 3)
         self.assertEqual(self.solution.lifeBoat([1, 2, 4, 5, 6, 7, 8, 8, 9, 11], 10), 7)
         self.assertEqual(self.solution.lifeBoat([1, 12, 14, 15, 16, 17, 18, 18, 19, 11], 10), 10)
+
+    def test_subsetSum(self):
+        self.assertEqual(self.solution.subsetSum([1,2,4],7), True)
+
 if __name__ == '__main__':
     unittest.main()
