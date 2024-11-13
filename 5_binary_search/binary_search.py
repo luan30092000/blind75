f124493c892daf8367f3f1b7175238ee23e4f53c
Tai Luan Nguyen <luan.nguyen@workstream.is>
2024-11-12 17:28:05 -0800

    add comment

--- 5_binary_search/binary_search.py
+++ 5_binary_search/binary_search.py
@@ -15,18 +15,15 @@ class Solution:
         res = nums[0]
         l, r = 0, len(nums) - 1
         
-        while l <= r:
-            # Shift only to the right => if shifted, right most always smaller than left most
-            if nums[r] > nums[l]:
-                return min(res, nums[l])
-            m = (l + r) // 2
-            res = min(res,nums[m])
-            if nums[m] >= nums[l]:
-                l = m + 1
-            else:
-                r = m - 1
+        while l <= r:   # generic condition
+            if nums[r] > nums[l]:   # found unshifted minimum portion -> max is right most, min is left most -> take left most
+                res = min(res, nums[l])
+                break
+            else:   # l to r portion is shifted -> max and min stand next to each other
+                m = (l + r) // 2
+                res = min(res,nums[m])
+                if nums[m] >= nums[l]:  # max is next to min -> max and min is on the right
+                    l = m + 1
+                else:   # max is next to min -> max and min is on the left
+                    r = m - 1
         return res
-
-
-
-
