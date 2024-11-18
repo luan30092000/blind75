505305c39f76b1b6cc8e23d469a801d2cd8f8360
Tai Luan Nguyen <luan.nguyen@workstream.is>
2024-11-17 23:33:10 -0800

    Update comment - added bigO runtime

--- 5_binary_search/binary_search.py
+++ 5_binary_search/binary_search.py
@@ -2,15 +2,7 @@ class Solution:
     def findMin(self, nums: list[int]) -> int:
         '''
         153. Find Minimum in Rotated Sorted Array
-        Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
-
-        [4,5,6,7,0,1,2] if it was rotated 4 times.
-        [0,1,2,4,5,6,7] if it was rotated 7 times.
-        Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
-
-        Given the sorted rotated array nums of unique elements, return the minimum element of this array.
-
-        You must write an algorithm that runs in O(log n) time.
+        bigO: log(N)
         '''
         res = nums[0]
         l, r = 0, len(nums) - 1
@@ -30,6 +22,7 @@ class Solution:
     def search(self, nums: list[int], target: int) -> int:
         '''
         33. Search in Rotated Sorted Array
+        bigO: log(N)
         '''
         l, r = 0, len(nums) - 1
         while l <= r:
